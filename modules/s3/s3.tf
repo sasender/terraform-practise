05a376a4c4e79880879dcb5f434a843353e48f53
sasender <sasender123@gmail.com>
2022-02-23 15:25:07 +0530

    s3

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,9 +1,10 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "my-tf-test-bucket-ggff"
-  acl    = "private"
+  #acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
+
