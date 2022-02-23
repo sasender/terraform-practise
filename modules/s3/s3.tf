04ed8cec47b22d93dfeb37ba79b663ca5f36d8ad
sasender <sasender123@gmail.com>
2022-02-23 15:51:47 +0530

    multiple

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -7,4 +7,15 @@ resource "aws_s3_bucket" "bucket" {
     Environment = "Dev"
   }
 }
+ #creating multiple buckets ;
+ variable "s3_bucket_name" {
+  type    = "list"
+  default = ["prod_bucket", "stage-bucket", "qa_bucket"]
+}
 
+resource "aws_s3_bucket" "henrys_bucket" {
+  count         = "${length(var.s3_bucket_name)}"
+  bucket        = "${var.s3_bucket_name[count.index]}"
+  acl           = "private"
+  force_destroy = "true"
+}
